0c0cbf695aec42fb80853adea82b55604c17f355
/script <65987444+FinnBaltazar1111@users.noreply.github.com>
2023-12-23 22:02:41 -0500

    Update and rename WordleBot.py to index.html

--- WordleBot.py
+++
@@ -1,6 +0,0 @@
-import datetime
-import requests
-date = datetime.date.today()
-url = f"https://www.nytimes.com/svc/wordle/v2/{date:%Y-%m-%d}.json"
-response = requests.get(url).json()
-print(f"Answer: {response['solution']}")
